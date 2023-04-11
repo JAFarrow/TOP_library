e59014d47a1fd39707a9b365d490efc4f638fd50
JAFarrow <f.justin@tutanota.com>
2023-04-11 21:27:07 +0200

    Book Card Addition Implemented Implemented logic in order to render form input as a card on the main view.

--- script.js
+++ script.js
@@ -1,18 +1,66 @@
 const formToggle = document.getElementById('addBookBtn');
 const additionOverlay = document.getElementById('inputOverlay')
+const mainPage = document.querySelector('main');
 
-function book(title, author, pages) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = true;
-    this.info = function() {
-        let readStatus = (this.read ? 'Read' : 'Not Read');
-        return `${this.title} by ${this.author}. ${this.pages} pages. ${readStatus}.`;
-    }
-};
+const bookTitleInput = document.getElementById('bookTitle');
+const bookAuthorInput = document.getElementById('bookAuthor');
+const bookPagesInput = document.getElementById('bookPages');
+const bookReadInput = document.getElementById('bookReadSlider');
+const submitBtn = document.getElementById('submitBtn');
+
+let myLibrary = [];
 
 formToggle.addEventListener('click', () => {
     additionOverlay.classList.remove('inputBoxHide');
     additionOverlay.classList.add('inputBoxShow');
 });
+
+function book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+};
+
+function cardCreation (obj) {
+    let cardDiv = document.createElement('div');
+    cardDiv.classList.add('bookCard');
+    let bookTitle = document.createElement('h3');
+    bookTitle.innerText = obj.title;
+    cardDiv.appendChild(bookTitle);
+    let bookAuthor = document.createElement('h3');
+    bookAuthor.innerText = obj.author;
+    cardDiv.appendChild(bookAuthor);
+    let bookPages = document.createElement('p');
+    bookPages.innerText = obj.pages;
+    cardDiv.appendChild(bookPages);
+    return cardDiv; 
+};
+
+function cardPush (card) {
+    mainPage.appendChild(card);
+}
+
+function customSubmit(event) {
+    myLibrary.push(new book(bookTitleInput.value, bookAuthorInput.value, bookPagesInput.value, bookReadInput.checked));
+    additionOverlay.classList.remove('inputBoxShow');
+    additionOverlay.classList.add('inputBoxHide');
+    console.log(myLibrary)
+    event.preventDefault();
+}
+
+submitBtn.addEventListener('click', customSubmit, false);
+
+cardPush(cardCreation({
+    "title": "Hello",
+    "author": "Benis",
+    "pages": "123",
+    "read": true
+}));
+
+cardPush(cardCreation({
+    "title": "Hello",
+    "author": "Benis",
+    "pages": "123",
+    "read": true
+}));
